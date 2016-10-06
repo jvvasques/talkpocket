b46d901eeddb7c381dd64072db64b79351389225
Joao Vasques <joao.vasques@talkdesk.com>
2016-10-06 21:44:46 +0100

    Add core.async logic to feed and watson
    In order to increase concurrency and also for personal fun, we decided
    to expose the APIs of the feed_extractor and Watson component using
    core.async channels. A uuid is generated uppon the HTTP request that
    will be used as the key for the final audio file

--- talkpocket-api/src/talkpocket_api/extractor/feed_extractor.clj
+++ talkpocket-api/src/talkpocket_api/extractor/feed_extractor.clj
@@ -1,13 +1,28 @@
 (ns talkpocket-api.extractor.feed-extractor
-  (:require [diffbot.core :refer :all]))
+  (:require [diffbot.core :refer :all]
+            [clojure.core.async
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread
+                     alts! alts!! timeout]]
+            [environ.core :refer [env]]))
 
-(def token (System/getenv "DIFFBOT_TOKEN"))
+(def token (env :diffbot-token))
 
-(defn extract
+(defn- extract
   "Extract article from URL"
-  [url]
-  (let [content (article token url)
+  [input]
+  (let [{url :url id :id} input
+        content (article token url)
         text (:text content)
         url (:url content)]
-    {:text text
-     :url url}))
+    (conj {:text text} input)))
+
+(defn consumer
+  "Consumes an url and returns the article from it in the out channel"
+  [in]
+  (let [out (chan)]
+    (go
+      (let [url (<! in)]
+        (>! out (conj (extract url) url))))
+    out))
+
