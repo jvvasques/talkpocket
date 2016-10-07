483a2658f6c0ffdbe51f8dae70f34c791127fd25
Jorge Rebocho <jorge.rebocho@talkdesk.com>
2016-10-07 16:06:59 +0100

    Initial version of the UI

--- ui/client/components/EntryRow/entryRow.js
+++ ui/client/components/EntryRow/entryRow.js
@@ -1,23 +1,21 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react'
 
-class EntryRow extends Component {
-
-  render() {
-    return (
-        <div>
-            asasds
-            // <span>Article: {articleUrl}</span>            
-            // <span>Sound: {soundUrl}</span>
-            // <span>Image: {image}</span>
-        </div>
-    );
-  }
+const EntryRow = ({articleUrl, soundUrl, image}) => {
+  
+  return (
+        <tr>
+            <td className="collapsing">
+                {articleUrl}
+            </td>
+            <td>Sound: {soundUrl}</td>  
+        </tr>
+    )
 }
 
 EntryRow.propTypes = {
     articleUrl: PropTypes.string.isRequired,
     soundUrl: PropTypes.string,
     image: PropTypes.string
-};
+}
 
-export default EntryRow;
+export default EntryRow
