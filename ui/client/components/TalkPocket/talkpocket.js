7cfd31017892480d0235be04620bf4642b8f2209
Jorge Rebocho <jorge.rebocho@talkdesk.com>
2016-10-08 12:20:03 +0100

    Added support for language translation

--- ui/client/components/TalkPocket/talkpocket.js
+++ ui/client/components/TalkPocket/talkpocket.js
@@ -9,11 +9,15 @@ class TalkPocket extends Component {
 
         this.state = {
             entries: [],
-            urlInput: 'sdasd'
+            urlInput: '',
+            language: 'pt',
+            translate: false
         };
         this.addNewEntry = this.addNewEntry.bind(this)
         this.updateEntriesStatus = this.updateEntriesStatus.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleLanguageChange = this.handleLanguageChange.bind(this)
+        this.handleTranslateCheck = this.handleTranslateCheck.bind(this)
     }
 
     updateEntriesStatus () {        
@@ -55,11 +59,31 @@ class TalkPocket extends Component {
         this.setState(newState)
     }
 
+    handleLanguageChange(event) {
+        const newState = Object.assign({}, this.state, {language: event.target.value})
+
+        this.setState(newState)
+    }
+
+    handleTranslateCheck(event) {
+        const newState = Object.assign({}, this.state, {translate: event.target.checked})
+
+        this.setState(newState)
+    }
+
     addNewEntry () {        
+        let requestData = {"url": this.state.urlInput }
+        if (this.state.translate)
+        {
+            requestData = Object.assign({}, requestData, {"lang": this.state.language})
+        }
+
+        console.log('Request data', requestData)
+
         $.ajax({
             url: "http://localhost:8080/talk",
             type: "POST",
-            data: JSON.stringify({"url": this.state.urlInput}),
+            data: JSON.stringify(requestData),
             contentType: "application/json",            
             success: function(){
                 console.log('sucess')
@@ -69,11 +93,9 @@ class TalkPocket extends Component {
             }.bind(this)
         })
 
-        // var entries = this.state.entries.push({ articleUrl: this.state.urlInput})
+        const newState = Object.assign({}, this.state, {urlInput: ''})
 
-        // const newState = Object.assign({}, this.state, entries)
-
-        // this.setState(newState)
+        this.setState(newState)
     }
 
     render() {
@@ -89,6 +111,15 @@ class TalkPocket extends Component {
                         onChange={this.handleChange} />
                     <button className="ui button" onClick={this.addNewEntry}>Get Audio</button>
                 </div>
+                <br />
+                <div className="ui checkbox">
+                    <input type="checkbox" name="translate" defaultChecked={this.state.translate} onChange={this.handleTranslateCheck}/>
+                    <label>Translate this text for me in &nbsp;</label>
+                </div>
+                <select value={this.state.language} onChange={this.handleLanguageChange}>
+                    <option value="it">Italian</option>                    
+                    <option value="pt">Portuguese</option>
+                </select>
                 <br /><br />                
                 <table className="ui celled striped table">
                     <thead>
